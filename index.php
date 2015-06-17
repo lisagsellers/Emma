283bb27fdbeffa9236fb111f873160fa019eea4e
lisagsellers <lsellers@PROVOST-WD00.uncanet.unca.edu>
2015-06-17 16:12:57 -0400

    SenateSubmit
    Senate Submit Application

---
+++ index.php
@@ -0,0 +1,10 @@
+  <!-- File: index.php -->
+  <!-- Purpose:  entry page for SenateSubmit website -->
+  <!-- Authors:  Marietta Cameron & Lisa Sellers>
+  <!-- Last Modified:  June 7, 2015  by Mec--> 
+<?php
+    include("templates/header.htm");
+    include("templates/openingSidebar.htm");	
+    include("templates/login.htm");
+    include("templates/footer.htm");
+?>
